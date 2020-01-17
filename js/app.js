f2522a3e66144c9f2661fe5a9297749ac119bbee
Omar Calderon <stivenomar7@gmail.com>
2020-01-17 11:43:16 -0500

    creacion de los contenedores de las tareas en cards y eliminacion de la interfaz mediante el boton delete

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 let form;
 let titleTask;
 let descriptionEvent;
+let sectionTasks;
 form = document.getElementById('formTask');
+sectionTasks = document.getElementById('tasks')
 
 form.addEventListener('submit',getDatesOfForm);
+sectionTasks.addEventListener('click',deleteTask)
 
 window.addEventListener('DOMContentLoaded',getTaskArrayOfLocalStorage)
 
@@ -28,14 +31,45 @@ function generateObjectTask(titleTask,descriptionEvent){
 
 
 function addTask(Task){
-    
+
     let arrayOfLocalStorage;
 
     arrayOfLocalStorage = getTaskArrayOfLocalStorage();
     arrayOfLocalStorage.push(Task);
+    drawTask(Task);
     localStorage.setItem('tasks',JSON.stringify(arrayOfLocalStorage));
 
-    console.log(arrayOfLocalStorage);
+}
+
+function drawTask(objectTask){
+
+    let card;
+    card = document.createElement('div');
+    card.setAttribute('class','card horizontal');
+
+    card.innerHTML = `
+    <div class="card-stacked">
+        <div class="card-action ">
+            <h6 class="titleCard">${objectTask.title}</h6>
+            <a class="waves-effect waves-light pink darken-1 btn right btnDeleteTask"><i
+                    class="material-icons">delete_forever</i></a>
+        </div>
+        <div class="divider"></div>
+        <div class="card-content">
+            <p>${objectTask.description}</p>
+        </div>
+    </div>
+    `
+
+    sectionTasks.appendChild(card);
+}
+
+function deleteTask(event){
+    
+    if(event.target.classList.contains('btnDeleteTask')){
+        console.log(event.target.parentElement.parentElement.parentElement.remove());
+    }
+
 }
 
 function getTaskArrayOfLocalStorage(){
